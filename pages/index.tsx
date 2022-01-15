89a6a6321be29541fbfdd831a31f0933dd4db9e7
SlyPuffin <daniel@nttsmc.com>
2022-01-15 18:23:23 +0900

    Made a simple flashcard front-side template with tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <div className="text-2xl">Hello World</div>;
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="relative flex h-4/6 w-4/6 bg-cyan-600 justify-center items-center border-slate-400 border-4">
+        <div className="text-3xl">Front Side</div>
+        <div className="absolute text-2xl bottom-1 left-2">Level [X--]</div>
+        <div className="absolute text-2xl bottom-1 right-2">
+          Source Material
+        </div>
+      </div>
+    </div>
+  );
 }
