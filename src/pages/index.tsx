038a54b68c940c84574c2cb5b244ff4b6728169d
Thomas Liebert <thomas.d.liebert@gmail.com>
2022-02-15 10:42:02 -0600

    changes to .env for working locally

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { trpc } from "@/utils/trpc";
 import React from "react";
 import Link from "next/link";
 import { DxDeck } from "@prisma/client";
+import { randomUUID } from "crypto";
 
 export default function Home() {
   const { data, isLoading } = trpc.useQuery(["get-decks"]);
@@ -14,7 +15,7 @@ export default function Home() {
   class Decks extends React.Component<DeckProps, DeckState> {
     render() {
       const listItems = this.props.decks.map((deck) => (
-        <Link href={`/review/${deck.id}`}>
+        <Link href={`/review/${deck.id}`} key={randomUUID}>
           <div className="cursor-pointer bg-orangeweboxfordblue-tertiary border-orangeweboxfordblue-border border-4 shadow-2xl text-4xl">
             <div className="text-4xl px-8 py-2 text-orangeweboxfordblue-primary">
               <li>{deck.name}</li>
