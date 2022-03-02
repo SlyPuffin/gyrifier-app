88adc0fb056518cf3b5c77ea6379288ebc8f8643
Thomas Liebert <thomas.d.liebert@gmail.com>
2022-03-02 15:43:05 -0600

    added practice and edit links to SingleDeck layout

--- src/layouts/SingleDeck.tsx
+++ src/layouts/SingleDeck.tsx
@@ -4,14 +4,26 @@ import Link from "next/link"
 export default function SingleDeck(props: any) {
     return (
         <div key={props.deck.id}>
-            <div className="editButton">
-            <Link href={`/decks/practice/${props.deck.id}`}>
-                <div className="cursor-pointer bg-orangeweboxfordblue-tertiary border-orangeweboxfordblue-border border-4 shadow-2xl text-4xl">
-                <div className="text-4xl px-8 py-2 text-orangeweboxfordblue-primary">
-                    <li>{props.deck.name}</li>
-                </div>
-                </div>
-            </Link>
+            <div className="reviewButton">
+                
+                    <div className="cursor-pointer bg-orangeweboxfordblue-tertiary border-orangeweboxfordblue-border border-4 shadow-2xl text-4xl">
+                    <div className="text-4xl px-8 py-2 text-orangeweboxfordblue-primary flex justify-between">
+                        <li>{props.deck.name}</li>
+                        <div className="flex">
+                                <Link href={`/decks/practice/${props.deck.id}`}>
+                                    <div className="w-10 h-10 rounded-full bg-red-400 flex justify-center">
+                                        <div>P</div>
+                                    </div>
+                                </Link>
+                                <Link href={`/decks/edit/${props.deck.id}`}>
+                                    <div className="w-10 h-10 rounded-full bg-red-400 flex justify-center">
+                                        <div>E</div>
+                                    </div>
+                                </Link>
+                        </div>
+                    </div>
+                    </div>
+
             </div>
         </div>
     )
