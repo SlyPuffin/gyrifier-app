38980146cf20b3cfd1248943c57cddf3269a9947
Thomas Liebert <thomas.d.liebert@gmail.com>
2022-03-02 14:20:04 -0600

    deleted review folder

--- src/pages/review/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function index() {
-  return (
-    <button className="bg-green-300 border-green-600 border-b p-4 m-4 rounded">Start Deck</button>
-  )
-}
