bd2142b5172b4ddbf8e038ad4a930886d41788a7
Thomas Liebert <thomas.d.liebert@gmail.com>
2022-03-02 14:18:25 -0600

    renamed review to decks and changed associated links:

--- src/pages/decks/index.tsx
+++ src/pages/decks/index.tsx
@@ -45,7 +45,7 @@ export default function Home() {
     class Decks extends React.Component<DeckProps, DeckState> {
       render() {
         return this.props.decks.map((deck) => (
-          <Link href={`/review/${deck.id}`} key={deck.id}>
+          <Link href={`/decks/${deck.id}`} key={deck.id}>
             <div className="cursor-pointer bg-orangeweboxfordblue-tertiary border-orangeweboxfordblue-border border-4 shadow-2xl text-4xl">
               <div className="text-4xl px-8 py-2 text-orangeweboxfordblue-primary">
                 <li>{deck.name}</li>
