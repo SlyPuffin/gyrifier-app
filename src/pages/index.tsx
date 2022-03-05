86ddd7dcae846253f469de0edd4d1c813f46f88d
nleech16 <nateleech@gmail.com>
2022-03-05 15:12:00 -0600

    Decks state in Home function and handleDeckSubmit (#16)
    * Decks state in Home function and handleDeckSubmit
    
    * Use decks concat
    
    Co-authored-by: Nathan Leech <nateleech@nathans-mbp.lan>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { trpc } from "@/utils/trpc";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { Deck } from "@prisma/client";
 
@@ -16,6 +16,24 @@ export default function Home() {
       refetchOnWindowFocus: false,
     }
   );
+  const addDeckMutation = trpc.useMutation('add-deck');
+
+  const [decks, setDecks] = React.useState<Deck[]>(data?.decks || []);
+
+  const handleDeckSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const name = (e as any).target.elements.name;
+
+    const input = {
+      name: name.value,
+      type: "default",
+      userId: userQuery.data?.user?.id || ''
+    };
+    const deck = await addDeckMutation.mutateAsync(input);
+    setDecks(decks.concat(deck));
+    name.value = '';
+  }
 
   type DeckProps = {
     decks: Deck[];
@@ -24,8 +42,8 @@ export default function Home() {
 
   class Decks extends React.Component<DeckProps, DeckState> {
     render() {
-      const listItems = this.props.decks.map((deck) => (
-        <Link href={`/review/${deck.id}`}>
+      return this.props.decks.map((deck) => (
+        <Link href={`/review/${deck.id}`} key={deck.id}>
           <div className="cursor-pointer bg-orangeweboxfordblue-tertiary border-orangeweboxfordblue-border border-4 shadow-2xl text-4xl">
             <div className="text-4xl px-8 py-2 text-orangeweboxfordblue-primary">
               <li>{deck.name}</li>
@@ -33,7 +51,6 @@ export default function Home() {
           </div>
         </Link>
       ));
-      return <ul>{listItems}</ul>;
     }
   }
 
@@ -52,7 +69,13 @@ export default function Home() {
     return (
       <div className="h-screen w-screen flex justify-center items-center">
         <div className="h-5/6 w-5/6 relative flex justify-center items-center">
-          <Decks decks={data.decks} />
+          <ul>
+            <form onSubmit={handleDeckSubmit}>
+              <input id="name" name="name" className="border-orangeweboxfordblue-border border-4 shadow-2xl text-4xl px-8 py-2 text-orangeweboxfordblue-primary" placeholder="New Deck"/>
+              <input type="submit" className="hidden"/>
+            </form>
+            <Decks decks={decks} />
+          </ul>
         </div>
       </div>
     );
