b22c63f45df220f5ab5b2339868cddbb15f2f96a
Thomas Liebert <thomas.d.liebert@gmail.com>
2022-04-14 11:08:53 -0500

    tweaks to extra whitespace and removal of withOpacityValue function in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,3 @@
-function withOpacityValue(variable) {
-  return ({ opacityValue }) => {
-    if (opacityValue === undefined) {
-      console.log(`rgb(var(${variable}))`)
-      return `rgb(var(${variable}))`
-    }
-    return `rgb(var(${variable}) / ${opacityValue})`
-  }
-}
-
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,css}"],
   theme: {
@@ -16,11 +6,10 @@ module.exports = {
         skin: {
           primary: "var(--primary-fill)",
           secondary: "var(--secondary-fill)",
-          "contrast": 'var(--contrast-fill)',
+          contrast: 'var(--contrast-fill)',
           muted: "var(--muted-fill)",
           test: "var(--test-red)",
           oxford: "#002147",
-          wowie: "var(--fun-zany)",
           special: "var(--bright-special)",
           logo: "var(--secondary-text)"
         }
