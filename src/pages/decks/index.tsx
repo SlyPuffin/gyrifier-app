809eae83c04a0ff24e9c72feddf95e3dffe0cd69
Nathan Leech <nateleech@nathans-mbp.lan>
2022-04-20 21:59:04 -0500

    Render new deck submit box when no decks exist yet

--- src/pages/decks/index.tsx
+++ src/pages/decks/index.tsx
@@ -54,12 +54,14 @@ export default function Home() {
     }
   }
 
-  if (isLoading || !data) {
+  if (isLoading) {
     return (
       <div className="h-screen w-screen flex justify-center items-center">
         <div className="h-5/6 w-5/6 relative flex justify-center items-center">
           <div className="cursor-pointer bg-orangeweboxfordblue-tertiary border-orangeweboxfordblue-border border-4 shadow-2xl text-4xl">
-            No Decks...
+            <div className="text-4xl px-8 py-2 text-orangeweboxfordblue-primary">
+              Loading...
+            </div>
           </div>
         </div>
       </div>
